a435ef8a7d2391975559eafacd222ea3087a2f1f
Lafen Lesley <lesleytech6@gmail.com>
2020-03-06 16:09:42 +0200

    Deployed

---
+++ docs/precache-manifest.aee1dd143125fcad3bdc4d0d78d5f589.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322f38475adff7f06d81990fc7b55017",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "52a67a3d28c6f05cf2f1",
+    "url": "/calculator/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6823e47d8ebd62306c6a",
+    "url": "/calculator/static/css/main.9d468e94.chunk.css"
+  },
+  {
+    "revision": "52a67a3d28c6f05cf2f1",
+    "url": "/calculator/static/js/2.4e18e8d0.chunk.js"
+  },
+  {
+    "revision": "98cef8f2c6f4a9493000de24ffc1680a",
+    "url": "/calculator/static/js/2.4e18e8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6823e47d8ebd62306c6a",
+    "url": "/calculator/static/js/main.59c19203.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/calculator/static/media/digital-7.1e670d88.ttf"
+  }
+]);
